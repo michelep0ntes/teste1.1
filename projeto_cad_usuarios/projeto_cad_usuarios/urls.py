cfd8f040a72ba7112d8e1455fe97df9b81340f10
michelep0ntes <pontesmichele03@gmail.com>
2024-03-22 14:04:17 -0300

    teste_django1

--- projeto_cad_usuarios/projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
+from django.contrib import admin
 from app_cad_usuarios.views import home
 urlpatterns = [
+    path("admin/", admin.site.urls),
     # rota, viewnresponsável, nome de referência
     #usuarios.com
     path('', home , name='home'),
     #usuarios.com/usuarios
-    path('usuarios/',views.usuarios,name='listagem_usuarios')
+   
 ]
